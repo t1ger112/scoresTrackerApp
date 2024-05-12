084d8eda57285d869ff8921b1e81e3dbb2ce2c8c
Freddie <155257406+t1ger112@users.noreply.github.com>
2024-05-12 23:00:52 +0100

    Delete scoresTrackerApp directory

--- scoresTrackerApp/scoresTrackerApp/containerFRM.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class containerFRM
-    Private Sub containerFRM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        homepageFRM.MdiParent = Me
-        welcomeFRM.MdiParent = Me
-        welcomeFRM.Show()
-
-    End Sub
-End Class
--- scoresTrackerApp/scoresTrackerApp/welcomeFRM.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class welcomeFRM
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        homepageFRM.Show()
-        Me.Hide()
-    End Sub
-End Class
